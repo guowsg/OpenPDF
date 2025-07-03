c2c2f8d79d1ae935591b75f98435bf86e900a0d0
guowsg <151120488+guowsg@users.noreply.github.com>
2025-07-03 16:23:30 +0800

    Update TrueTypeFont.java
    新增更新说明

--- openpdf/src/main/java/com/lowagie/text/pdf/TrueTypeFont.java
+++ openpdf/src/main/java/com/lowagie/text/pdf/TrueTypeFont.java
@@ -287,6 +287,9 @@ class TrueTypeFont extends BaseFont {
                 .endsWith(".ttc")) {
             process(ttfAfm, forceRead);
             if (!justNames && embedded && os_2.fsType == 2) {
+                //修改说明：2025年6月修改： 防止加载字体异常
+                //原作者：OpenPdf
+                //本文件基于LGPL V2.1许可分发
                 // throw new DocumentException(
                 //         MessageLocalization.getComposedMessage("1.cannot.be.embedded.due.to.licensing.restrictions",
                 //                 fileName + style));
